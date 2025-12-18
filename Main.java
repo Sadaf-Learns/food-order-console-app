dd8110e0a4a248df96c98c913deac2ea36a47d77
Sadaf-Learns <sadafaboonasr@gmail.com>
2025-12-18 11:57:11 +0330

    Initial commit - food order console app

--- Main.java
+++ Main.java
@@ -9,143 +9,143 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         ArrayList<Order> orders = new ArrayList<>();
         while (true){
-            System.out.println("1-ثبت سفارش");
-            System.out.println("2-مشاهده سفارش");
-            System.out.println("3-ویرایش سفارش");
-            System.out.println("4-حذف شفارش");
-            System.out.println("0-خروج");
-            System.out.println("عدد مورد نظر راوارد کنید:");
+            System.out.println("1-Create order");
+            System.out.println("2-View order");
+            System.out.println("3-Edit order");
+            System.out.println("4-Delete order");
+            System.out.println("0-Exit");
+            System.out.println("Please enter your choice:");
             int choice=scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
                 case 1:
-                    System.out.println("گزینه1-ثبت سفارش");
-                    System.out.println("نام غذا را وارد کنید.");
+                    System.out.println("Option1- Create order");
+                    System.out.println("Enter food name");
                     String foodName = scanner.nextLine();
-                    System.out.println("آیا سفارش بیرون بر است؟(y/n) :");
+                    System.out.println("Is this order for delivery?(y/n): ");
                     String deliveryInput = scanner.nextLine();
                     boolean isDelivery = deliveryInput.equalsIgnoreCase("y");
                     String address = "";
                     if (isDelivery) {
                         while (true) {
-                            System.out.println("آدرس را وارد کنید: ");
+                            System.out.println("Enter delivery address: ");
                             address = scanner.nextLine();
                             if (!address.isEmpty()) {
                                 break;
                             }
-                            System.out.println("آدرس نمیتواند خالی باشد.");
+                            System.out.println("Address cannot be empty.");
                         }
                     }
-                    System.out.println("ساعت و تاریخ تحویل را مشخص کنید: (yyyy-MM-ddTHH:mm) ");
+                    System.out.println("Enter delivery date and time (yyyy-MM-ddTHH:mm): ");
                     String deliveryTimeInput=scanner.nextLine();
                     LocalDateTime deliveryDateTime=LocalDateTime.parse(deliveryTimeInput);
                     int id = orders.size() + 1;
                     Order order = createOrder(id,foodName,isDelivery,address,deliveryDateTime); //R
                     orders.add(order);
-                    System.out.println("سفارش با شناسه " + id + "ثبت شد! ");
+                    System.out.println("Order with ID " + id + "has been successfully created.");
 
                     break;
                 case 2:
-                    System.out.println("گزینه2-مشاهده سفارش");
-                    System.out.println("شناسه سفارش را وارد کنید: ");
+                    System.out.println("Option2- View order");
+                    System.out.println("Enter order ID: ");
                     int searchId = scanner.nextInt();
                     scanner.nextLine();
                     Order foundorder = findOrderId(orders,searchId); //R
 
                     if (foundorder != null) {
-                        System.out.println("...اطلاعات سفارش...");
-                        System.out.println("شناسه:" + foundorder.id);
-                        System.out.println("نام غذا:" + foundorder.foodName);
-                        System.out.println("تاریخ ثبت: " + foundorder.date);
-                        System.out.println("بیرون بر: " + (foundorder.isDelivery ? "بله" : "خیر"));
+                        System.out.println("------Order Details------");
+                        System.out.println("ID: " + foundorder.id);
+                        System.out.println("Food Name: " + foundorder.foodName);
+                        System.out.println("Order Date: " + foundorder.date);
+                        System.out.println("Delivery: " + (foundorder.isDelivery ? "Yes" : "No"));
                         if (foundorder.isDelivery) {
-                            System.out.println("آدرس:" + foundorder.address);
+                            System.out.println("Delivery Address: " + foundorder.address);
                         }
                         System.out.println("..........");
                     } else {
-                        System.out.println("سفارشی با این شناسه پیدا نشد.");
+                        System.out.println("Order not found.");
                     }
                     break;
                 case 3:
-                    System.out.println("گزینه3-ویرایش سفارش");
-                    System.out.println("شناسه سفارشی که میخواهید ادیت کنید وارد کنید: ");
+                    System.out.println("Option3- Edit order");
+                    System.out.println("Enter the order ID you want to edit: ");
                     int editId = scanner.nextInt();
                     scanner.nextLine();
                     Order editOrder = findOrderId(orders,editId); //R
                     if (editOrder == null) {
-                        System.out.println("سفارشی بااین شناسه یافت نشد. ");
+                        System.out.println("Order not found. ");
                         break;
                     }
                     java.time.LocalDateTime registerTime = java.time.LocalDateTime.parse(editOrder.dateTime);
                     java.time.LocalDateTime now = java.time.LocalDateTime.now();
                     long minutesPassed = java.time.Duration.between(registerTime, now).toMinutes();
                     if (minutesPassed > 10) {
-                        System.out.println("زمان زیادی گذشته وامکان ویرایش نیست.");
+                        System.out.println("Too much time has passed. Editing is not allowed. ");
                         break;
                     }
-                    System.out.println("چه چیزی را می‌خواهید ویرایش کنید؟");
-                    System.out.println("1- نام غذا");
-                    System.out.println("2- وضعیت بیرون‌بر");
-                    System.out.println("3- آدرس");
-                    System.out.println("0- انصراف");
+                    System.out.println("What do you want to edit?");
+                    System.out.println("1- Food Name");
+                    System.out.println("2- Delivery status");
+                    System.out.println("3- Delivery address");
+                    System.out.println("0- Cancel");
                     int editChoice = scanner.nextInt();
                     scanner.nextLine();
                     switch (editChoice) {
                         case 1:
-                            System.out.println("نام غذای جدید وارد کنید: ");
+                            System.out.println("Enter new food name: ");
                             editOrder.foodName = scanner.nextLine();
-                            System.out.println("نام غذا تغییر کرد.");
+                            System.out.println("Food name updated successfully. ");
                             break;
                         case 2:
-                            System.out.println("آیا غذا بیرون بر باشد؟(y/n)");
+                            System.out.println("Is it Delivery?(y/n): ");
                             String d = scanner.nextLine();
                             editOrder.isDelivery = d.equalsIgnoreCase("y");//Y=y,N=n(equalsignorecase)
                             if (editOrder.isDelivery) {
-                                System.out.println("ادرس جدید را وارد کنید: ");
+                                System.out.println("Enter new address: ");
                                 editOrder.address = scanner.nextLine();
                             } else {
                                 editOrder.address = "";
                             }
 
-                    System.out.println("وضعیت بیرون بر تغییر کرد. ");
+                    System.out.println("Delivery status updated.");
                     break;
                 case 3:
                     if (!editOrder.isDelivery) {
-                        System.out.println("سفارش بیرون بر نیست. ");
+                        System.out.println("This order is not delivery.");
                         break;
                     }
-                    System.out.println("آدرس جدید را وارد کنید: ");
+                    System.out.println("Enter new address: ");
                     editOrder.address = scanner.nextLine();
-                    System.out.println("آدرس تغییر کرد!");
+                    System.out.println("Address update successfully.");
                     break;
                 case 0:
-                    System.out.println("انصراف");
+                    System.out.println("Operation canceled.");
                     break;
                 default:
-                    System.out.println("انتخاب نامعتبر است.");
+                    System.out.println("Invalid choice.");
             }
             break;
                 case 4:
-                    System.out.println("گزینه4-حذف سفارش");
-                    System.out.println("شناسه سفارشی که میخواهید حذف کنید را وارد کنید: ");
+                    System.out.println("Option4- Delete order");
+                    System.out.println("Enter Id you want to delete: ");
                     int deleteId=scanner.nextInt();
                     scanner.nextLine();
                     Order deleteOrder = findOrderId(orders,deleteId); //R
 
                     if (deleteOrder == null) {
-                        System.out.println("سفارشی بااین شناسه پیدا نشد.");
+                        System.out.println("Order not found.");
                         break;
                     }
                     orders.remove(deleteOrder); //orders arraylist=> remove(obj)
-                    System.out.println( "سفارش شناسه "+ deleteId+"حذف شد ");
+                    System.out.println( "Order with ID"+ deleteId+"has been deleted.");
                     break;
                 case 5:
-                    System.out.println("زمان تحویل را مشخص کنید");
+                    System.out.println("Enter delivery time: ");
                 case 0:
-                    System.out.println("خروج از برنامه");
+                    System.out.println("Exit");
                     return;
                 default:
-                    System.out.println("بین بازه 0-4 انتخاب شود");
+                    System.out.println("Choose between 0 and 5.");
 
 
                 }
